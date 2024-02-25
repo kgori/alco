704ae3d81d444caa2246f5fccffdd1513060e26b
Kevin Gori <kcg25@cam.ac.uk>
2024-02-25 18:50:51 +0000

    Premature git commit in 3.. 2.. 1.. (I hope this fix works - hey why not write some tests, lazy?)

--- src/processing.rs
+++ src/processing.rs
@@ -3,10 +3,11 @@ use rust_htslib::bam::{self, Read};
 use crate::base_counter::BaseCounter;
 use crate::variant::Variant;
 use core::ops::Index;
+use crate::errors::AcError;
 
 pub fn process_batch(bam: &mut bam::IndexedReader, positions: &Vec<Variant>) -> Result<(), Box<dyn Error>> {
     if positions.is_empty() {
-        return Ok(());
+        return Err(Box::new(AcError { message: "Wasn't expecting an empty batch...".to_string() }))
     }
 
     // Determine the minimum and maximum positions
@@ -15,41 +16,53 @@ pub fn process_batch(bam: &mut bam::IndexedReader, positions: &Vec<Variant>) -> 
     let max_pos = positions.last().map(|p| p.pos_one_based()).unwrap();
 
     bam.fetch((chr, min_pos, max_pos))?;
-
     let mut pileup = bam.pileup();
-    let mut pileup_col = pileup.next().ok_or("No next position in pileup")??;
 
-    // Process pileup for each CSV record in the batch
-    for position in positions.iter() {
-        let ref_id = &position.chr;
-        let pos_0based = position.pos_zero_based();
-        let pos_1based = position.pos_one_based();
-        let refbase = position.refbase;
-        let altbase = position.altbase;
-        let mut counts = BaseCounter::new(refbase, altbase);
+    if let Some(result) = pileup.next() {
+        let mut pileup_col = result?;
 
-        if pileup_col.pos() > pos_0based {
-            continue;
-        }
+        // Process pileup for each CSV record in the batch
+        for position in positions.iter() {
+            let ref_id = &position.chr;
+            let pos_0based = position.pos_zero_based();
+            let pos_1based = position.pos_one_based();
+            let refbase = position.refbase;
+            let altbase = position.altbase;
+            let mut counts = BaseCounter::new(refbase, altbase);
 
-        while pileup_col.pos() < pos_0based {
-            pileup_col = pileup.next().ok_or("No next position in pileup")??;
-        }
+            if pileup_col.pos() > pos_0based {
+                println!("{ref_id}\t{pos_1based}\t{refbase}\t{altbase}\t{counts}");
+                continue;
+            }
 
-        if pileup_col.pos() == pos_0based {
-            for aln in pileup_col.alignments() {
-                let mapping_quality = aln.record().mapq();
-                let qpos = aln.qpos();
-                if let Some(i) = qpos {
-                    let base_qual = aln.record().qual()[i];
-                    let base = *aln.record().seq().index(i) as char;
-                    if mapping_quality > 30 && base_qual > 15 {
-                        counts.add(base);
+            while pileup_col.pos() < pos_0based {
+                pileup_col = pileup.next().ok_or("No next position in pileup")??;
+            }
+
+            if pileup_col.pos() == pos_0based {
+                for aln in pileup_col.alignments() {
+                    let mapping_quality = aln.record().mapq();
+                    let qpos = aln.qpos();
+                    if let Some(i) = qpos {
+                        let base_qual = aln.record().qual()[i];
+                        let base = *aln.record().seq().index(i) as char;
+                        if mapping_quality > 30 && base_qual > 15 {
+                            counts.add(base);
+                        }
                     }
                 }
             }
+            println!("{ref_id}\t{pos_1based}\t{refbase}\t{altbase}\t{counts}");
         }
-        println!("{ref_id}\t{pos_1based}\t{refbase}\t{altbase}\t{counts}");
+    } else {
+        for position in positions.iter() {
+            let ref_id = &position.chr;
+            let pos_1based = position.pos_one_based();
+            let refbase = position.refbase;
+            let altbase = position.altbase;
+            let counts = BaseCounter::new(refbase, altbase);
+            println!("{ref_id}\t{pos_1based}\t{refbase}\t{altbase}\t{counts}");
+        }
     }
     Ok(())
 }
